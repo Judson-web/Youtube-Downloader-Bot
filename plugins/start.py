41f8f59b19bb849607efaa8d030b4b02087ba21e
KING XV <85796566+Judson-web@users.noreply.github.com>
2021-09-13 16:09:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "🎥Story Time channel Files 🎥", url="https://t.me/storytimeoG")]
     ])
-    welcomed = f"Hy <b>{message.from_user.first_name}</b>\n💁🏻‍♂️ How To Use Me? Just Send Me Any YouTube Video Link To Download The Video (Playlist Isn't Supported)!        Made With ❤️ By @PRIMER_ROOM🔥"
+    welcomed = f"Hy <b>{message.from_user.first_name}</b>\n💁🏻‍♂️ How To Use Me? Just Send Me Any YouTube Video Link To Download The Video (Playlist Isn't Supported)!                     Made With ❤️ By @PRIMER_ROOM🔥"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
