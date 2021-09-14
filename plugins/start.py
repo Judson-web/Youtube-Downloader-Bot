da8467d664f1297315f834e1a3b98f72c7b70490
KING XV <85796566+Judson-web@users.noreply.github.com>
2021-09-14 16:11:45 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("PRIMER ROOM SERIES UPDATES MOVIES🍿🍿", url="https://t.me/PRIMER_ROOM")],
         [InlineKeyboardButton(
-            "😇Add Me To Your Group😇", url="t.me/{}?startgroup=true".format(bot.username)) ]]))
+            "😇Add Me To Your Group😇", url="https://t.me/storytimeoG%22)]
     ])
     welcomed = f"Hy <b>{message.from_user.first_name}</b>\n💁🏻‍♂️ How To Use Me? Just Send Me Any YouTube Video Link To Download The Video (Hit /help for more details)!                                           Made With ❤️ By @PRIMER_ROOM🔥"
     await message.reply_text(welcomed, reply_markup=joinButton)
